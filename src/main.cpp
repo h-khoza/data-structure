d6449fa2617fc69085b502b4611be2a151ccf11f
Hendry Khoza <h3khoza@gmail.com>
2020-07-30 20:06:41 +0200

    mv main to src

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+// LEARNING ABOUT C++ VECTORES
+// Author Hendry Khoza
+//
+//
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+int main() {
+    string str = "Okay this is test"
+
+}
