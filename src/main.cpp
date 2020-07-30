ef6eef6dc2cc76ad8bfc84928fe1bdfde651a05a
Hendry Khoza <h3khoza@gmail.com>
2020-07-30 22:08:23 +0200

    learn about c++ vectors

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,36 @@
 
 
 int main() {
-    string str = "Okay this is test"
+    std::vector<int> myVector;
+    unsigned count;
+    int enteredVal;
+    const char QUIT = 'n';
+    char quitChar;
+    int position;
 
+    std::cout << "Do you want to enter a value? y or n ";
+    std::cin >> quitChar;
+
+    while(quitChar != QUIT)  {
+        std::cout << "Enter an integer ";
+        std::cin >> enteredVal;
+
+        myVector.push_back(enteredVal);
+
+        std::cout << "Do you want to enter a value? y or n ";
+        std::cin >> quitChar;
+    }
+
+    std::cout << "Size of the list is " << myVector.size() << std::endl;
+    std::cout << "The list: " << std::endl;
+
+    for (count = 0; count < myVector.size(); ++count)
+        std::cout << " " << myVector[count] << std::endl;
+
+    std::cout << "Enter a position you wanna dispaly ";
+    std::cin >> position;
+    std::cout << "The item at position " << position << " is: ";
+    std::cout << myVector.at(position) << std::endl;
+
+    return 0;
 }
